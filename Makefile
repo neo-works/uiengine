5f9133cc847a2a404719cd74e53b14d180978b52
neo <nerosoft@outlook.com>
2023-05-13 16:21:14 +0800

    TDD is better

--- Makefile
+++ Makefile
@@ -6,6 +6,15 @@ SRCS = src/demo.c src/web/html_document.c
 demo: $(SRCS)
 	$(CC) $(CFLAGS) -o demo src/demo.c src/web/html_document.c
 
+test: $(SRCS)
+	$(CC) $(CFLAGS) -o test src/test.c src/web/html_document.c
+
+runDemo: demo
+	./demo
+
+runTest: test
+	./test
+
 .PHONY: clean
 clean:
-	rm demo
+	rm demo test
--- src/demo.c
+++ src/demo.c
@@ -1,15 +1,5 @@
 #include <stdio.h>
-#include "../include/assert.h"
-#include "../include/web/html_document.h"
 
 int main(int argc, char* argv[]) {
-    struct HtmlDocument* document = document_load("<body id=\"test\">sss</body>");
-    ASSERT_NOT_NULL(document);
-
-    struct HtmlElement* element = document->get_element_by_id("test");
-
-    ASSERT_NOT_NULL(element);
-    ASSERT_STR_EQUAL(element->id, "test");
-
     return 0;
 }

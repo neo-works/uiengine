ecf6f8c20fc2aad8b3734439cc615877b80cc252
neo <nerosoft@outlook.com>
2023-05-16 00:33:24 +0800

    HTML parser: tag

--- src/test.c
+++ src/test.c
@@ -2,12 +2,6 @@
 #include "../include/assert.h"
 #include "../include/web/html_document.h"
 
-DEFINE_TEST_CASE(document_should_not_be_null) {
-    struct HtmlDocument* document = document_load("<body id=\"test\">sss</body>");
-    ASSERT_NOT_NULL(document);
-    return 0;
-}
-
 DEFINE_TEST_CASE(element_id_should_equal) {
     struct HtmlDocument* document = document_load("<body id=\"test\">sss</body>");
     struct HtmlElement* element = document->get_element_by_id(document, "test");
@@ -16,17 +10,54 @@ DEFINE_TEST_CASE(element_id_should_equal) {
     return 0;
 }
 
-DEFINE_TEST_CASE(document_consume_whitespace) {
+DEFINE_TEST_CASE(document_should_consume_whitespace) {
     char *doc = "          <body></body>";
     doc = document_consume_whitespace(doc);
     ASSERT_STR_EQUAL(doc, "<body></body>");
     return 0;
 }
 
+DEFINE_TEST_CASE(document_should_match_and_consume) {
+    char *doc = "<body></body>";
+    doc = document_match_and_consume(doc, "<");
+    ASSERT_STR_EQUAL(doc, "body></body>");
+    doc = document_match_and_consume(doc, "body");
+    ASSERT_STR_EQUAL(doc, "></body>");
+    doc = document_match_and_consume(doc, ">");
+    ASSERT_STR_EQUAL(doc, "</body>");
+    doc = document_match_and_consume(doc, "</");
+    ASSERT_STR_EQUAL(doc, "body>");
+    doc = document_match_and_consume(doc, "body");
+    ASSERT_STR_EQUAL(doc, ">");
+    doc = document_match_and_consume(doc, ">");
+    ASSERT_STR_EQUAL(doc, "\0");
+    return 0;
+}
+
+DEFINE_TEST_CASE(document_should_parse_tag) {
+    char *doc = "body></body>";
+    char *tag = document_parse_tag(doc);
+    ASSERT_STR_EQUAL(tag, "body");
+    return 0;
+}
+
+DEFINE_TEST_CASE(document_should_parse_tag_2) {
+    char *doc = "<body>sss</body>";
+    HtmlDocument *document = document_load(doc);
+    ASSERT_NOT_NULL(document);
+    ASSERT_NOT_NULL(document->body);
+    ASSERT_NOT_NULL(document->body->tag);
+    ASSERT_STR_EQUAL(document->body->tag, "body");
+    return 0;
+}
+
 int main(int argc, char* argv[]) {
     // RUN_ALL_TESTS();
-    TEST_document_should_not_be_null();
-    TEST_document_consume_whitespace();
+    TEST_document_should_consume_whitespace();
+    TEST_document_should_match_and_consume();
+    TEST_document_should_parse_tag();
+    TEST_document_should_parse_tag_2();
+    
     TEST_element_id_should_equal();
     return 0;
 }
--- src/web/html_document.c
+++ src/web/html_document.c
@@ -15,19 +15,48 @@ void document_init(HtmlDocument *document) {
     document->get_element_by_name = document_default_get_element_by_name;
 }
 
+static iswhitespace(char c) {
+    return (c == ' ') || (c == '\t');
+}
+
+static isalpha(char c) {
+    return ((c >= 'a') && (c <= 'z')) || 
+           ((c >= 'A') && (c <= 'Z'));
+}
+
 char* document_consume_whitespace(char *doc) {
-    while(*doc == ' ' || *doc == '\t') {
+    while (iswhitespace(*doc)) {
         doc++;
     }
     return doc;
 }
 
 char *document_match_and_consume(char *doc, char *tok) {
-
+    char *tok_tmp = tok;
+    while ((*doc != '\0') && (*tok_tmp != '\0')) {
+        if (*doc == *tok_tmp) {
+            doc++;
+            tok_tmp++;
+        } else {
+            break;
+        }
+    }
+    return doc;
 }
 
 char *document_parse_str(char *doc) {
-
+    char *tag = mem_alloc(0x20);
+    memset(tag, 0, 0x20);
+    uint32_t idx = 0;
+    while ((*doc != '\0')) {
+        if (isalpha(*doc)) {
+            tag[idx++] = *doc;
+            doc++;
+        } else {
+            break;
+        }
+    }
+    return tag;
 }
 
 char *document_parse_tag(char *doc) {
@@ -35,16 +64,22 @@ char *document_parse_tag(char *doc) {
 }
 
 HtmlElement *document_parse_childrens(char *doc) {
-    
+    return NULL;
 }
 
 HtmlAttribute *document_parse_attributes(char *doc) {
-
+    return NULL;
 }
 
+/**
+ * @brief parse element
+ * <{TAG} {ATTRIBUTES}>{CHILDRENS}</{TAG}>
+ * @param doc 
+ * @return HtmlElement* 
+ */
 HtmlElement *document_parse_element(char *doc) {
-    doc = document_consume_whitespace(doc);
     HtmlElement *element = (HtmlDocument *)mem_alloc(sizeof(HtmlDocument));
+    doc = document_consume_whitespace(doc);
     doc = document_match_and_consume(doc, "<");
     element->tag = document_parse_tag(doc);
     element->attributes = document_parse_attributes(doc);
@@ -66,8 +101,6 @@ void document_parse(HtmlDocument *document, char *doc) {
 struct HtmlDocument *document_load(const char *doc) {
     HtmlDocument *document = (HtmlDocument*)mem_alloc(sizeof(HtmlDocument));
     document_init(document);
-
     document_parse(document, doc);
-
     return document;
 }

08e6c7d9cc7694bb7ebbdc293acfd9d9e9bc7f7f
neo <nerosoft@outlook.com>
2023-05-28 19:01:30 +0800

    feat:(render) mouse event

--- src/demo.c
+++ src/demo.c
@@ -10,23 +10,23 @@
 int main(int argc, char* argv[]) {
     char *doc =
     "<body id=\"test_id\">"
-        "<button width=\"100\" height=\"60\" x=\"10\" y=\"10\" background=\"rgb(10,20,30)\">text</button>"
-        "<button width=\"100\" height=\"60\" x=\"120\" y=\"10\" background=\"rgb(70,20,30)\">text</button>"
-        "<button width=\"100\" height=\"60\" x=\"230\" y=\"10\" background=\"rgb(10,50,30)\">text</button>"
-        "<button width=\"100\" height=\"60\" x=\"340\" y=\"10\" background=\"rgb(10,20,210)\">text</button>"
-        "<button width=\"100\" height=\"60\" x=\"450\" y=\"10\" background=\"rgb(50,20,30)\">text</button>"
+        "<button width=\"100\" height=\"60\" x=\"10\" y=\"10\" background=\"rgb(255,255,255)\">text</button>"
+        "<button width=\"100\" height=\"60\" x=\"120\" y=\"10\" background=\"rgb(255,255,255)\">text</button>"
+        "<button width=\"100\" height=\"60\" x=\"230\" y=\"10\" background=\"rgb(255,255,255)\">text</button>"
+        "<button width=\"100\" height=\"60\" x=\"340\" y=\"10\" background=\"rgb(255,255,255)\">text</button>"
+        "<button width=\"100\" height=\"60\" x=\"450\" y=\"10\" background=\"rgb(255,255,255)\">text</button>"
 
-        "<button width=\"100\" height=\"60\" x=\"10\" y=\"100\" background=\"rgb(10,65,30)\">text</button>"
-        "<button width=\"100\" height=\"60\" x=\"120\" y=\"100\" background=\"rgb(98,56,30)\">text</button>"
-        "<button width=\"100\" height=\"60\" x=\"230\" y=\"100\" background=\"rgb(88,20,30)\">text</button>"
-        "<button width=\"100\" height=\"60\" x=\"340\" y=\"100\" background=\"rgb(10,132,30)\">text</button>"
-        "<button width=\"100\" height=\"60\" x=\"450\" y=\"100\" background=\"rgb(10,232,12)\">text</button>"
+        "<button width=\"100\" height=\"60\" x=\"10\" y=\"100\" background=\"rgb(255,255,255)\">text</button>"
+        "<button width=\"100\" height=\"60\" x=\"120\" y=\"100\" background=\"rgb(255,255,255)\">text</button>"
+        "<button width=\"100\" height=\"60\" x=\"230\" y=\"100\" background=\"rgb(255,255,255)\">text</button>"
+        "<button width=\"100\" height=\"60\" x=\"340\" y=\"100\" background=\"rgb(255,255,255)\">text</button>"
+        "<button width=\"100\" height=\"60\" x=\"450\" y=\"100\" background=\"rgb(255,255,255)\">text</button>"
 
-        "<button width=\"100\" height=\"60\" x=\"10\" y=\"190\" background=\"rgb(43,20,30)\">text</button>"
-        "<button width=\"100\" height=\"60\" x=\"120\" y=\"190\" background=\"rgb(10,97,30)\">text</button>"
-        "<button width=\"100\" height=\"60\" x=\"230\" y=\"190\" background=\"rgb(43,231,30)\">text</button>"
-        "<button width=\"100\" height=\"60\" x=\"340\" y=\"190\" background=\"rgb(211,121,30)\">text</button>"
-        "<button width=\"100\" height=\"60\" x=\"450\" y=\"190\" background=\"rgb(33,20,122)\">text</button>"
+        "<button width=\"100\" height=\"60\" x=\"10\" y=\"190\" background=\"rgb(255,255,255)\">text</button>"
+        "<button width=\"100\" height=\"60\" x=\"120\" y=\"190\" background=\"rgb(255,255,255)\">text</button>"
+        "<button width=\"100\" height=\"60\" x=\"230\" y=\"190\" background=\"rgb(255,255,255)\">text</button>"
+        "<button width=\"100\" height=\"60\" x=\"340\" y=\"190\" background=\"rgb(255,255,255)\">text</button>"
+        "<button width=\"100\" height=\"60\" x=\"450\" y=\"190\" background=\"rgb(255,255,255)\">text</button>"
     "</body>";
     HtmlDocument* document = document_load(doc);
     document->dump(document);
